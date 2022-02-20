6b60f91416b8d56d060868400ff0d48e12c4d289
Bence Papp <bencepapp1101@gmail.com>
2022-02-20 18:05:11 +0100

    header created

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,8 +1,47 @@
+import Image from "next/image";
 import { FC } from "react";
 import s from "./Header.module.scss";
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UsersIcon,
+} from "@heroicons/react/solid";
 
 const Header: FC = () => {
-  return <div>Enter</div>;
+  return (
+    <header className={s.header}>
+      <div className={s.header_logo}>
+        <Image
+          src="https://links.papareact.com/qd3"
+          alt="LOGO"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+
+      <div className={s.header_search}>
+        <input
+          className={s.header_search_input}
+          type="text"
+          placeholder="Search"
+        />
+        <SearchIcon className={s.header_search_icon} />
+      </div>
+
+      <div className={s.header_usernav}>
+        <p>Become a host</p>
+        <GlobeAltIcon className={s.header_usernav_globealticon} />
+
+        <div className={s.header_usernav_menu}>
+          <MenuIcon className={s.header_usernav_menu_menuicon} />
+          <UserCircleIcon className={s.header_usernav_menu_usercircleicon} />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
